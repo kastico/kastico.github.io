2e7135ac798b3eff2736eb7d0599f205d96c84be
kastico <kastico.stash@gmail.com>
2025-10-20 00:55:31 +0100

    vault backup: 2025-10-20 00:55:31

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://kastico.github.io',
-})
+  vite: {
+    resolve: {
+      alias: {
+        '@components': '/src/components',
+        '@': '/src',
+      }
+    }
+  }
+});
